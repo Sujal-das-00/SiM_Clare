97ca62f49046cccee3614a7f00ba8c28626a4f46
Sujal-das-00 <dassujal276@gmail.com>
2026-02-19 23:05:02 +0530

    database connected to cloud for test

--- config/db.js
+++ config/db.js
@@ -7,21 +7,25 @@ const production = process.env.NODE_ENV === "production";
 
 const pool = mysql.createPool({
     host: production ? process.env.DB_HOST : process.env.DEV_DB_HOST,
+    port: production?process.env.DB_PORT:process.env.DEV_DB_PORT,
     user: production ? process.env.DB_USER : process.env.DEV_DB_USER,
     password: production ? process.env.DB_PASSWORD : process.env.DEV_DB_PASSWORD,
     database: production ? process.env.DB_NAME : process.env.DEV_DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error("Database connection failed:", err.message);
-        return;
-    }
-    console.log("Database Connected");
+try {
+    const connection = await pool.getConnection();
+    console.log("Database Connected Successfully");
     connection.release();
-});
+} catch (err) {
+    console.error("Database connection failed:", err.message);
+}
+
 
 export default pool;
