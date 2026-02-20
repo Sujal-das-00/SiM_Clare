1fcfeccdd11a26c5d419f8b190f358452bb3b489
Sujal-das-00 <dassujal276@gmail.com>
2026-02-20 11:55:58 +0530

    resend otp logic

--- controllers/resendOtp.js
+++ controllers/resendOtp.js
@@ -1,4 +1,6 @@
 import { ismailExistService } from "../models/ismailExistService.js";
+import { sendMail } from "../services/mailer.js";
+import { generateAndSaveOtp } from "../services/otpGrenerator.js";
 import { handelResponse } from "../utils/errorHandeler.js";
 
 
@@ -7,7 +9,12 @@ export const resendOtp = async (req,res,next)=>{
         const {email} = req.body;
         if(!email) return handelResponse(res,404,"Please provide the email ")
         const ismailExist = await ismailExistService(email);
-        if(!ismailExist){}
+        if(!ismailExist) return handelResponse(res,404,"Email not found please do signup");
+
+        const user_id = ismailExist.user_id;
+        const otp = await generateAndSaveOtp(user_id,'EMAIL_VERIFICATION')
+        const response = await sendMail(email,otp);
+        return handelResponse(res,200,"otp sent succcessfully",response.envelope.to[0])
     } catch (error) {
         next(error)
     }
--- models/ismailExistService.js
+++ models/ismailExistService.js
@@ -3,7 +3,7 @@ export const ismailExistService = async(email)=>{
     try {
         const [rows] = await db.query("Select id from users where email=?",[email])
         if(rows.length === 0) return false;
-        return({status:true,user_id:rows.id})
+        return({status:true,user_id:rows[0].id})
     } catch (error) {
         throw error;
     }
