df61b36d3f322eff65fdf1831fd3d547d45c714e
Sujal-das-00 <dassujal276@gmail.com>
2026-02-21 00:40:24 +0530

    fixed mail in port

--- services/mailer.js
+++ services/mailer.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, 
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, 
-    },
+        pass: process.env.EMAIL_PASS,
+    }, tls: {
+        family: 4 
+    }
 });
 
 export const sendMail = async (to, otp) => {
